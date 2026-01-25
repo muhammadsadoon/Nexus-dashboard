13cd701e07b741574acbff3b87ab5c1a06b5414d
muhammadsadoon <muhammadsadoonsohail786@gmail.com>
2026-01-25 17:19:30 +0500

    setup the envoriment

---
+++ src/pages/notfound/notfoud.jsx
@@ -0,0 +1,12 @@
+import { Camera } from 'lucide-react'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default NotFound
